28e8bbaad1f4f90f5dee955f16f8ef0e38226b22
imedisa <medisayep@gmail.com>
2023-03-29 15:54:28 +0330

    i fixed navbar in mobile form

--- script/app.js
+++ script/app.js
@@ -1,13 +1,18 @@
 const navBtn =document.querySelector(".nav__btn")
+const navMenu =document.querySelector(".nav-menu")
+
 let navOpen=false;
 navBtn.addEventListener("click",function (){
-    navBtn.classList.add("nav__btn--open")
+    
     if(navOpen){
         navBtn.classList.remove("nav__btn--open")
-        navOpen= false
+        navMenu.classList.remove("nav-menu--open")
+        navOpen=false
+        
     }
     else{
         navBtn.classList.add("nav__btn--open")
+        navMenu.classList.add("nav-menu--open")
         navOpen=true
     }
 })
